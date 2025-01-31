ac28ccd1a5fca933b3465990db3cd3f179a181ef
Jalen Mendoza <54971623+jalenm872@users.noreply.github.com>
2025-01-31 11:06:23 -0500

    Player rotation when moving
    Working on player animation next

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,7 +16,6 @@ public class Player : MonoBehaviour
         if(Input.GetKey(KeyCode.A)){
             inputVector.x = -1;
         }
-
         if(Input.GetKey(KeyCode.D)){
             inputVector.x = +1;
         }
@@ -24,8 +23,14 @@ public class Player : MonoBehaviour
         //Make sure the vector is normalized
         inputVector.Normalize();
 
+        //Move the player
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float RotateSpeed = 10f;
+
+        //Rotate the player to face the direction of movement with Slerp
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * RotateSpeed);
 
         //Debug.Log(inputVector);
     }
